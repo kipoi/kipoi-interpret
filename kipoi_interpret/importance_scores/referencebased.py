e7bf4fec989947028872164bfbed3e7e7027138a
Roman Kreuzhuber <rkreuzh@gmail.com>
2018-07-17 08:59:12 +0100

    added TAL1 model to tests. added alpha version of DeepLIFT integration

--- kipoi_interpret/importance_scores/referencebased.py
+++ kipoi_interpret/importance_scores/referencebased.py
@@ -1,6 +1,8 @@
 from __future__ import division, absolute_import, print_function
 from .base import ImportanceScoreWRef
 from .gradient import Gradient
+from kipoi.model import KerasModel, TensorFlowModel
+from kipoi.data_utils import numpy_collate
 
 import tempfile
 import numpy as np
@@ -10,18 +12,18 @@ import numpy as np
 class DeepLift(ImportanceScoreWRef):
 
     def __init__(self, model, output_layer,
-                 task_idx, preact, mxts_mode = 'deeplift'):
-
-        import deeplift
+                 task_idx, preact, mxts_mode = 'rescale_conv_revealcancel_fc',
+                 batch_size = 32):
         from deeplift.conversion import kerasapi_conversion as kc
         from deeplift.layers import NonlinearMxtsMode
 
         def get_mxts_mode(mode_name):
-            mxts_modes = {'deeplift': NonlinearMxtsMode.DeepLIFT_GenomicsDefault,
-                          'revealcancel': NonlinearMxtsMode.RevealCancel,
-                          'rescale': NonlinearMxtsMode.Rescale,
-                          'gradient': NonlinearMxtsMode.Gradient,
-                          'guidedbackprop': NonlinearMxtsMode.GuidedBackprop}
+            # Labels from examples:
+            mxts_modes = {'rescale_conv_revealcancel_fc': NonlinearMxtsMode.DeepLIFT_GenomicsDefault,
+                          'revealcancel_all_layers': NonlinearMxtsMode.RevealCancel,
+                          'rescale_all_layers': NonlinearMxtsMode.Rescale,
+                          'grad_times_inp': NonlinearMxtsMode.Gradient,
+                          'guided_backprop': NonlinearMxtsMode.GuidedBackprop}
             return mxts_modes[mode_name]
 
         # TODO: create and return the deeplift func, which
@@ -31,16 +33,25 @@ class DeepLift(ImportanceScoreWRef):
             raise Exception("Model not compatible with DeepLift")
 
         self.task_idx = task_idx
-
+        self.batch_size = batch_size
 
         weight_f = tempfile.mktemp()
         arch_f = tempfile.mktemp()
         model.model.save_weights(weight_f)
         with open(arch_f, "w") as ofh:
             ofh.write(model.model.to_json())
-
         self.deeplift_model = kc.convert_model_from_saved_files(weight_f, json_file=arch_f,
                                                                 nonlinear_mxts_mode=get_mxts_mode(mxts_mode))
+        input_names = self.model._get_feed_input_names()
+        self.input_layer_idxs = []
+        self.output_layers_idxs = []
+        for input_name in input_names:
+            input_layer_name = input_name[:-len("_input")] if input_name.endswith("_input") else input_name
+            for i, l in enumerate(self.model.model.layers):
+                if l.name == input_layer_name:
+                    self.input_layer_idxs.append(i)
+
+        self.fwd_predict_fn = None
 
         # Compile the function that computes the contribution scores
         # For sigmoid or softmax outputs, target_layer_idx should be -2 (the default)
@@ -49,7 +60,7 @@ class DeepLift(ImportanceScoreWRef):
         # (which simply refers to the last layer)
         # If you want the DeepLIFT multipliers instead of the contribution scores, you can use get_target_multipliers_func
         self.deeplift_contribs_func = self.deeplift_model.get_target_contribs_func(
-            find_scores_layer_idx=0,
+            find_scores_layer_idx=self.input_layer_idxs,
             target_layer_idx=output_layer)
 
 
@@ -63,7 +74,11 @@ class DeepLift(ImportanceScoreWRef):
             # Support only keras models
             return False
         # Check the Keras backend
+        import keras
         import keras.backend as K
+
+        if not keras.__version__.startswith("2.0."):
+            return False
 
         # TODO - check the Keras version
         if model.backend is None:
@@ -73,18 +88,62 @@ class DeepLift(ImportanceScoreWRef):
         return backend == "tensorflow"
 
     def score(self, input_batch, input_ref):
+        #from deeplift.util import run_function_in_batches
         x_standardized = self.model._batch_to_list(input_batch)
+        ref_standaradized = None
+        if input_ref is not None:
+            ref_standaradized = self.model._batch_to_list(input_ref)
 
-        # Taken from DeepLIFT github readme
         scores = self.deeplift_contribs_func(task_idx=self.task_idx,
                                                  input_data_list=x_standardized,
-                                                 batch_size=10,
+                                                 input_references_list = ref_standaradized,
+                                                 batch_size=self.batch_size,
                                                  progress_update=1000)
 
+        """
+        # run_function_in_batches fails for 
+        scores = run_function_in_batches(
+            func=self.deeplift_contribs_func,
+            input_data_list=x_standardized,
+            batch_size=self.batch_size,
+            progress_update=1000,
+            task_idx=self.task_idx)
+        """
+
+        # DeepLIFT returns all samples as a list of individual samples
+        scores = [numpy_collate(el) for el in scores]
 
         # re-format the list-type input back to how the input_batch was:
         scores = self.model._match_to_input(scores, input_batch)
         return scores
+
+    def predict_on_batch(self, input_batch):
+        from deeplift.util import run_function_in_batches
+        from deeplift.util import compile_func
+        x_standardized = self.model._batch_to_list(input_batch)
+        if self.fwd_predict_fn is None:
+            # identify model output layers:
+            self.output_layers_idxs = []
+            for output_name in self.model.model.output_names:
+                for i, l in enumerate(self.model.model.layers):
+                    if l.name == output_name:
+                        self.output_layers_idxs.append(i)
+            inputs = [self.deeplift_model.get_layers()[i].get_activation_vars() for i in self.input_layer_idxs]
+            outputs = [self.deeplift_model.get_layers()[-1].get_activation_vars()]
+            deeplift_prediction_func = compile_func(inputs,outputs)
+
+        preds = run_function_in_batches(
+            input_data_list=x_standardized,
+            func=deeplift_prediction_func,
+            batch_size=self.batch_size,
+            progress_update=None)
+
+        preds = np.array(preds)
+        if len(self.output_layers_idxs) == 1:
+            preds = preds[0,...]
+
+        return preds
+
 
 
 class IntegratedGradients(ImportanceScoreWRef, Gradient):
---
+++ tests/test_referencebased.py
@@ -0,0 +1,72 @@
+from kipoi_interpret.importance_scores.referencebased import DeepLift
+import kipoi
+import pytest
+import config
+
+INSTALL_REQ = config.install_req
+from kipoi.pipeline import install_model_requirements
+import json
+from kipoi.utils import cd
+from tqdm import tqdm
+from kipoi import writers, readers
+import numpy as np
+import os
+
+
+predict_activation_layers = {
+    "rbp": "concatenate_6",
+    "tal1_model": "dense_1"
+    # "pyt": "3"  # two before the last layer
+}
+
+
+def test_deeplift():
+    #return True
+    example = "tal1_model"
+    layer = predict_activation_layers[example]
+    example_dir = "tests/models/{0}".format(example)
+    if INSTALL_REQ:
+        install_model_requirements(example_dir, "dir", and_dataloaders=True)
+
+    model = kipoi.get_model(example_dir, source="dir")
+    # The preprocessor
+    Dataloader = kipoi.get_dataloader_factory(example_dir, source="dir")
+    #
+    with open(example_dir + "/example_files/test.json", "r") as ifh:
+        dataloader_arguments = json.load(ifh)
+
+    for k in dataloader_arguments:
+        dataloader_arguments[k] = "example_files/" + dataloader_arguments[k]
+
+    d = DeepLift(model, output_layer=-2, task_idx=0, preact=None, mxts_mode = 'grad_times_inp')
+
+    new_ofname = model.source_dir + "/example_files/deeplift_grads_pred.hdf5"
+    if os.path.exists(new_ofname):
+        os.unlink(new_ofname)
+
+    writer = writers.HDF5BatchWriter(file_path=new_ofname)
+
+    with cd(model.source_dir):
+        dl = Dataloader(**dataloader_arguments)
+        it = dl.batch_iter(batch_size=32, num_workers=0)
+        # Loop through the data, make predictions, save the output
+        for i, batch in enumerate(tqdm(it)):
+            # make the prediction
+            pred_batch = d.score(batch['inputs'], None)
+
+            # Using Avanti's recommendation to check whether the model conversion has worked.
+            pred_batch_fwd = d.predict_on_batch(batch['inputs'])
+            orig_pred_batch_fwd = model.predict_on_batch(batch['inputs'])
+            assert np.all(pred_batch_fwd == orig_pred_batch_fwd)
+
+        output_batch = batch
+        output_batch["input_grad"] = pred_batch
+        writer.batch_write(output_batch)
+    writer.close()
+
+    new_res = readers.HDF5Reader.load(new_ofname)
+    ref_res = readers.HDF5Reader.load(model.source_dir + "/example_files/grads.hdf5")
+    assert np.all(np.isclose(new_res['input_grad'], (ref_res['inputs'] * ref_res['grads'])))
+
+    if os.path.exists(new_ofname):
+        os.unlink(new_ofname)
